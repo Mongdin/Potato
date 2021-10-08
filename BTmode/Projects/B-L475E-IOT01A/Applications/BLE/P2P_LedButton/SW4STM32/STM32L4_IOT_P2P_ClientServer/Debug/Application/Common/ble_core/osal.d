dffd55c53ef968942f9c580f8775ca074e1909cb
Mongdin <ajfqhkh@gmail.com>
2021-10-08 21:01:09 +0900

    backup
    why

---
+++ BTmode/Projects/B-L475E-IOT01A/Applications/BLE/P2P_LedButton/SW4STM32/STM32L4_IOT_P2P_ClientServer/Debug/Application/Common/ble_core/osal.d
@@ -0,0 +1,5 @@
+Application/Common/ble_core/osal.o: \
+ D:/git/Potato/BTmode/Projects/B-L475E-IOT01A/Applications/BLE/Common/ble_core/osal.c \
+ D:/git/Potato/BTmode/Projects/B-L475E-IOT01A/Applications/BLE/Common/ble_core/osal.h
+
+D:/git/Potato/BTmode/Projects/B-L475E-IOT01A/Applications/BLE/Common/ble_core/osal.h:
---
+++ normalmode/Projects/B-L475E-IOT01A/Applications/WiFi/WiFi_Client_Server/SW4STM32/B-L475E-IOT01/Debug/Application/User/crc.d
@@ -0,0 +1,3 @@
+Application/User/crc.o: ../Application/User/crc.c ../../../Inc/crc.h
+
+../../../Inc/crc.h:
